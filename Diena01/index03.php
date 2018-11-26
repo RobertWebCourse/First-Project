f839cd3050f23ad939599abe6a14ba1c1eeb17f8
RobertWebCourse <robert.19973@mail.ru>
2018-11-26 20:37:40 +0200

    PHP 'Diena01'

---
+++ Diena01/index03.php
@@ -0,0 +1,15 @@
+<?php 
+	$x = 10;
+	$y = 7;
+	$result1 = $x + $y;
+	$result2 = $x - $y;
+	$result3 = $x * $y;
+	$result4 = $x / $y;
+	$result5 = $x % $y;
+
+	echo $x.' + '.$y.' = '.$result1.'<br>';
+	echo $x.' - '.$y.' = '.$result2.'<br>';
+	echo $x.' * '.$y.' = '.$result3.'<br>';
+	echo $x.' / '.$y.' = '.$result4.'<br>';
+	echo $x.' % '.$y.' = '.$result5.'<br>';
+?>

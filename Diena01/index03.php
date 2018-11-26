16f90a6d9dbdbb79bf3a98143af25933c844b777
RobertWebCourse <robert.19973@mail.ru>
2018-11-26 21:37:53 +0200

    PHP 'Diena01'

--- Diena01/index03.php
+++ Diena01/index03.php
@@ -1,15 +1,24 @@
 <?php 
 	$x = 10;
 	$y = 7;
-	$result1 = $x + $y;
-	$result2 = $x - $y;
-	$result3 = $x * $y;
-	$result4 = $x / $y;
-	$result5 = $x % $y;
 
-	echo $x.' + '.$y.' = '.$result1.'<br>';
-	echo $x.' - '.$y.' = '.$result2.'<br>';
-	echo $x.' * '.$y.' = '.$result3.'<br>';
-	echo $x.' / '.$y.' = '.$result4.'<br>';
-	echo $x.' % '.$y.' = '.$result5.'<br>';
+	$result = $x + $y;
+
+	echo $x."+ ".$y." = ".$result."<br>";
+
+	$result = $x - $y;
+
+	echo $x."- ".$y." = ".$result."<br>";
+
+	$result = $x * $y;
+
+	echo $x."* ".$y." = ".$result."<br>";
+
+	$result = $x / $y;
+
+	echo $x."/ ".$y." = ".$result."<br>";
+
+	$result = $x % $y;
+
+	echo $x."% ".$y." = ".$result."<br>";
 ?>

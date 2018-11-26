2a149fa4538bce2bb3d405bb40d26326f67e5c4b
RobertWebCourse <robert.19973@mail.ru>
2018-11-26 20:21:45 +0200

    PHP 'Diena 01'

---
+++ Diena01/index01.php
@@ -0,0 +1,20 @@
+<?php 
+	$salis = 'Lietuva'; 
+	$miestas = 'Vilnius';
+	$populiacija = 564221;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Diena01</title>
+	<ul>
+		<li><?php echo $salis ?></li>
+		<li><?php echo $miestas ?></li>
+		<li><?php echo $populiacija ?></li>
+	</ul>
+</head>
+<body>
+	
+</body>
+</html>

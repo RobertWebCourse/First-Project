50381c97cda536a2c928b6e94262be9d56cf40ba
RobertWebCourse <robert.19973@mail.ru>
2018-12-02 22:14:28 +0200

    PHP Diena05 'index04'

--- Diena05/index04.php
+++ Diena05/index04.php
@@ -1,11 +1,11 @@
 <?php
 
-	$date = new DateTime("08/12/2012");
+	$date = new DateTime("08/12/2018");
 	$now = new DateTime();
 
 	if($date -> format('Y') < $now -> format('Y')) {
-  	  echo $date -> format('Y').' - '.$now -> format('Y');
+  	  echo '&copy; '.$date -> format('Y').' - '.$now -> format('Y');
 	}else {
-		echo $now -> format('Y');
+		echo '&copy; '.$now -> format('Y');
 	}
 ?>

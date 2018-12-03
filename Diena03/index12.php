b35ae501d4c10f80e2e994242247251ddc3cc8a8
RobertWebCourse <robert.19973@mail.ru>
2018-12-03 15:45:03 +0200

    PHP Diena 03 'index12'

--- Diena03/index12.php
+++ Diena03/index12.php
@@ -1,12 +1,9 @@
-<?php 
-	function stacia() {
-		$a = $_GET['name'];
-		$b = $_GET['surname'];
-		if($a && $b) {
-			echo $result = 'Vardas: '.$a.'<br>'.'Pavarde: '.$b;
-			exit();
-		}
-		stacia();
+<?php
+	$a = $_GET['name'];
+	$b = $_GET['surname'];
+	if($a && $b) {
+		echo 'Vardas: '.$a.'<br>'.'Pavarde: '.$b;
+		exit();
 	}
 ?>
 <!DOCTYPE html>
@@ -16,7 +13,7 @@
 	<title>Form</title>
 </head>
 <body>
-	<form action="<?php $result ?>" method="GET">
+	<form action="" method="GET">
 		<label><p>Name:</p><input type="text" name = "name"></label><label>
 		<label><p>Surname:</p><input type="text" name = "surname"></label><label> 
 		<input type="submit" value="get result">
